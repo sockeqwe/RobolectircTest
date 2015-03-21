dab1cf616b5d0f2ea36dd5b40c39ab0608558a41
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2015-03-22 00:21:35 +0100

    Updated to robolectric 3.0-SNAPSHOT

---
+++ lib/src/test/java/com/example/lib/CustomRobolectricRunner.java
@@ -0,0 +1,19 @@
+package com.example.lib;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+
+public class CustomRobolectricRunner extends RobolectricTestRunner {
+
+    public CustomRobolectricRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+        String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
+        String intermediatesPath = BuildConfig.class.getResource("").toString().replace("file:", "");
+        intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+
+        System.setProperty("android.package", BuildConfig.APPLICATION_ID);
+        System.setProperty("android.manifest", intermediatesPath + "/manifests/full/" + buildVariant + "/AndroidManifest.xml");
+        System.setProperty("android.resources", intermediatesPath + "/res/" + buildVariant);
+        System.setProperty("android.assets", intermediatesPath + "/assets/" + buildVariant);
+    }
+}
--- lib/src/test/java/com/example/lib/MyFragmentTest.java
+++ lib/src/test/java/com/example/lib/MyFragmentTest.java
@@ -1,18 +1,29 @@
 package com.example.lib;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
-import org.robolectric.util.FragmentTestUtil;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
 
+@RunWith(CustomRobolectricRunner.class)
+@Config(emulateSdk = 21, reportSdk = 21)
 public class MyFragmentTest {
 
   @Test public void testAppFragmentStart() {
     final TestFragment fragment = new TestFragment();
 
-    FragmentTestUtil.startFragment(fragment, FragmentActivity.class); // startVisibleFragment
+    ActivityController controller = Robolectric.buildActivity(FragmentActivity.class);
+    FragmentActivity activity = (FragmentActivity) controller.create().start().resume().visible().get();
+
+    FragmentManager manager = activity.getSupportFragmentManager();
+    manager.beginTransaction()
+        .add(fragment, "fragmentTag").commit();
 
     // My test examples - hamcrest matchers
     Assert.assertThat(fragment, CoreMatchers.not(CoreMatchers.nullValue()));
